343eb00670837dbdc9f99c36326a0684f7a6ca01
BarokDG <it.barok.dagim@gmail.com>
2022-04-25 23:47:27 +0300

    Refactor SearchSong

--- src/components/input/SearchSong.tsx
+++ src/components/input/SearchSong.tsx
@@ -46,30 +46,13 @@ export const SearchSong = ({ handleSubmit, handleSkip, guesses }) => {
             placeholder="Know it? Search artist or the song."
             onChange={filterValidGuesses}
           />
-          <ul className="absolute bottom-full left-0 flex flex-col mb-3 w-full formide-y divide-gray-500 cursor-pointer">
-            {matchInput.map(({ song }, index) => {
-              let classes =
-                'flex items-center px-4 py-1 bg-gray-200 mb-0.5 last:mb-0 text-gray-800 dark:bg-gray-600 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700'
-
-              classes +=
-                guesses.includes(song) && song !== solution.song
-                  ? ' pointer-events-none bg-rose-200 text-rose-800 dark:bg-rose-300 dark:text-rose-800'
-                  : ''
-
-              return (
-                <li key={index} className={classes} onClick={changeInput}>
-                  {guesses.includes(song) && song !== solution.song ? (
-                    <XIcon className="h-4 w-4 dark:stroke-rose-800 mr-3" />
-                  ) : (
-                    ''
-                  )}
-                  {song}
-                </li>
-              )
-            })}
-          </ul>
+          <SongOptions
+            matchInput={matchInput}
+            guesses={guesses}
+            changeInput={changeInput}
+          />
           <XIcon
-            className="w-5 h-5 absolute top-[50%] translate-y-[-50%] right-4 invisible stroke-gray-400 cursor-pointer"
+            className="w-5 h-5 absolute top-[50%] translate-y-[-50%] right-4 stroke-gray-400 cursor-pointer"
             onClick={() => {
               if (inputRef.current) {
                 inputRef.current.value = ''
@@ -80,5 +63,32 @@ export const SearchSong = ({ handleSubmit, handleSkip, guesses }) => {
         <ActionBar handleSkip={handleSkip} inputRef={inputRef} />
       </form>
     </>
+  )
+}
+
+function SongOptions({ matchInput, guesses, changeInput }) {
+  return (
+    <ul className="absolute bottom-full left-0 flex flex-col mb-3 w-full formide-y divide-gray-500 cursor-pointer">
+      {matchInput.map(({ song }, index) => {
+        let classes =
+          'flex items-center p-4 bg-gray-200 mb-0.5 last:mb-0 text-gray-800 dark:bg-gray-600 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700'
+
+        classes +=
+          guesses.includes(song) && song !== solution.song
+            ? ' pointer-events-none bg-rose-200 text-rose-800 dark:bg-rose-300 dark:text-rose-800'
+            : ''
+
+        return (
+          <li key={index} className={classes} onClick={changeInput}>
+            {guesses.includes(song) && song !== solution.song ? (
+              <XIcon className="h-4 w-4 dark:stroke-rose-800 mr-3" />
+            ) : (
+              ''
+            )}
+            {song}
+          </li>
+        )
+      })}
+    </ul>
   )
 }
