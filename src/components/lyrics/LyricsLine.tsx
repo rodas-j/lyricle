7f9e4e50cc395fb03a3769a3ab3a44b5592dfb88
rodas-j <se.rodas.workicho@gmail.com>
2022-04-29 15:55:39 -0400

    Removed border to make it slick

--- src/components/lyrics/LyricsLine.tsx
+++ src/components/lyrics/LyricsLine.tsx
@@ -6,7 +6,7 @@ export const LyricsLine = ({ sliceLyrics = 1 }) => {
       {solution.lyrics.slice(0, sliceLyrics).map((line, index) => (
         <div
           key={index}
-          className="line-reveal p-2 md:p-4 md:text-xl text-gray-800 bg-gray-200 border-2 border-gray-300 pointer-events-none select-none dark:bg-gray-800 dark:border-gray-500 dark:text-gray-100 text-center mb-2"
+          className="line-reveal p-2 md:p-4 md:text-xl text-gray-800 bg-gray-200 border-gray-300 pointer-events-none select-none dark:bg-gray-800 dark:border-gray-500 dark:text-gray-100 text-center mb-2"
         >
           {line}
         </div>
