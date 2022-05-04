9bb3eae578f117a64bddfbda4249625dc2ed3ac5
BarokDG <it.barok.dagim@gmail.com>
2022-05-04 13:45:01 +0300

    Add how to play modal

--- src/components/modals/HowToPlayModal.tsx
+++ src/components/modals/HowToPlayModal.tsx
@@ -5,18 +5,14 @@ type Props = {
   handleClose: () => void
 }
 
-export const InfoModal = ({ isOpen, handleClose }: Props) => {
+export const HowToPlayModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="How to Play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-100">
         Guess the song in 6 tries. After each guess, the color of the
         corresponding section of the progress bar will change to show if your
         guess was correct or not.
       </p>
-
-      <p className="text-sm text-gray-500 dark:text-gray-300"></p>
-
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300"></p>
     </BaseModal>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,29 +2,38 @@ import {
   ChartBarIcon,
   CogIcon,
   InformationCircleIcon,
+  QuestionMarkCircleIcon,
 } from '@heroicons/react/outline'
 import { GAME_TITLE } from '../../constants/strings'
 
 type Props = {
   setIsInfoModalOpen: (value: boolean) => void
+  setIsHowToPlayModalOpen: (value: boolean) => void
   setIsStatsModalOpen: (value: boolean) => void
   setIsSettingsModalOpen: (value: boolean) => void
 }
 
 export const Navbar = ({
   setIsInfoModalOpen,
+  setIsHowToPlayModalOpen,
   setIsStatsModalOpen,
   setIsSettingsModalOpen,
 }: Props) => {
   return (
     <div className="navbar">
       <div className="navbar-content px-5">
-        <InformationCircleIcon
-          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
-          onClick={() => setIsInfoModalOpen(true)}
-        />
+        <div className="flex">
+          <InformationCircleIcon
+            className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
+            onClick={() => setIsInfoModalOpen(true)}
+          />
+          <QuestionMarkCircleIcon
+            className="h-6 w-6 cursor-pointer dark:stroke-white"
+            onClick={() => setIsHowToPlayModalOpen(true)}
+          />
+        </div>
         <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
-        <div className="right-icons">
+        <div className="flex">
           <ChartBarIcon
             className="h-6 w-6 mr-3 cursor-pointer dark:stroke-white"
             onClick={() => setIsStatsModalOpen(true)}
