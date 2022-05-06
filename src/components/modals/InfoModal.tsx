8a023678e0d2642a55d96ce9158795b2d2c2f213
BarokDG <it.barok.dagim@gmail.com>
2022-05-06 11:51:31 +0300

    Add about page, update link preview thumbnail

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -1,5 +1,8 @@
 import { BaseModal } from './BaseModal'
 
+import { ReactComponent as Twitter } from '../../assets/twitter.svg'
+import { ReactComponent as Instagram } from '../../assets/instagram.svg'
+
 type Props = {
   isOpen: boolean
   handleClose: () => void
@@ -8,15 +11,54 @@ type Props = {
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-sm text-gray-500 dark:text-gray-100">
-        Guess the song in 6 tries. After each guess, the color of the
-        corresponding section of the progress bar will change to show if your
-        guess was correct or not.
+      <div className="w-full h-0 pb-[56%] relative mt-3 mb-6 pointer-events-none">
+        <iframe
+          className="giphy-embed absolute"
+          title="another-one-gif"
+          src="https://giphy.com/embed/qgri3D9sTwCUGMcT8L"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p className="text-sm dark:text-gray-200 mb-6">
+        We're sure you're all familiar with the "-le" type games by now. We were{' '}
+        <span className="decoration-double line-through">heavily</span> inspired
+        by both{' '}
+        <a
+          className="underline decoration-dashed decoration-green-400 dark:decoration-green-200 decoration-2 hover:bg-green-200 hover:text-gray-900 px-1"
+          href="https://heardle.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Heardle
+        </a>{' '}
+        and{' '}
+        <a
+          className="underline decoration-dashed decoration-green-400 dark:decoration-green-200 decoration-2 hover:bg-green-200 hover:text-gray-900 px-1"
+          href="https://www.nytimes.com/games/wordle/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Wordle
+        </a>{' '}
+        to create Lyricle.
       </p>
 
-      <p className="text-sm text-gray-500 dark:text-gray-300"></p>
+      <p className="text-sm dark:text-gray-200 mb-6">Hope you enjoy!</p>
 
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300"></p>
+      <div className="flex justify-center">
+        <div className="icon flex mr-3">
+          <a href="https://twitter.com/lyricle_app">
+            <Twitter className="w-6 h-6 mx-1 grayscale hover:grayscale-0" />
+          </a>
+          <a href="https://www.instagram.com/lyricle_app/">
+            <Instagram className="w-6 h-6 mx-1 grayscale hover:grayscale-0" />
+          </a>
+        </div>
+        <p className="dark:text-gray-200">@lyricle_app</p>
+      </div>
     </BaseModal>
   )
 }
