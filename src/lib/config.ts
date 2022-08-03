b63cc0275c079a8718da1207cb7a54f49e6749d3
rodas-j <se.rodas.workicho@gmail.com>
2022-08-03 07:59:31 -0700

    Added 70s with config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,11 @@ let name: string
 let gameState: string
 let gameStats: string
 switch (pathName) {
+  case '/70s':
+    key = '70s'
+    name = 'Lyricle 70s'
+
+    break
   case '/80s':
     key = '80s'
     name = 'Lyricle 80s'
--- src/lib/songs.ts
+++ src/lib/songs.ts
@@ -1,11 +1,15 @@
 import SONG_CHOICES_ALL from '../constants/lyricle.json'
 import SONG_CHOICES_80S from '../constants/80s.json'
+import SONG_CHOICES_70s from '../constants/70s.json'
 import { decadesConfig } from './config'
 import { mapArtistToSongs } from '../constants/validGuesses'
 
 let SONG_CHOICES = SONG_CHOICES_ALL
 
 switch (decadesConfig.key) {
+  case '70s':
+    SONG_CHOICES = SONG_CHOICES_70s
+    break
   case '80s':
     SONG_CHOICES = SONG_CHOICES_80S
     break
