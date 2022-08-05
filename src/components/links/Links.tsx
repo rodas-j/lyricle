1f91a325775414677e8fd1f07b36b5e5ae2cca4a
BarokDG <it.barok.dagim@gmail.com>
2022-08-06 01:21:57 +0300

    Refactor links

--- src/components/links/Links.tsx
+++ src/components/links/Links.tsx
@@ -1,30 +1,24 @@
 export const Links = () => {
   return (
     <div className="flex flex-wrap gap-4">
-      <a
-        href="https://decades.lyricle.app/70s"
-        className="flex-grow md:hover:scale-110 md:hover:transition-transform"
-      >
-        <article className="flex items-center justify-center p-4 bg-gradient-to-r   from-[#C6426E]/95 to-[#642B73]/95 max-w-s aspect-video rounded-md">
-          <h1 className="text-indigo-50 text-2xl font-bold">Lyricle 70s</h1>
-        </article>
-      </a>
-      <a
-        href="https://decades.lyricle.app/80s"
-        className="flex-grow md:hover:scale-110 md:hover:transition-transform"
-      >
-        <article className="flex items-center justify-center p-4 bg-gradient-to-r  from-[#f5af19]/95 to-[#f12711]/95 max-w-s aspect-video rounded-md">
-          <h1 className="text-indigo-50 text-2xl font-bold">Lyricle 80s</h1>
-        </article>
-      </a>
-      <a
-        href="https://decades.lyricle.app/90s"
-        className="flex-grow md:hover:scale-110 md:hover:transition-transform"
-      >
-        <article className="flex items-center justify-center p-4 bg-gradient-to-r  from-[#45a247]/95 to-[#283c86]/95 max-w-s aspect-video rounded-md">
-          <h1 className="text-indigo-50 text-2xl font-bold">Lyricle 90s</h1>
-        </article>
-      </a>
+      <Link era="70s" gradientStart="#C6426E" gradientEnd="#642B73" />
+      <Link era="80s" gradientStart="#f5af19" gradientEnd="#f12711" />
+      <Link era="90s" gradientStart="#45a247" gradientEnd="#283c86" />
     </div>
+  )
+}
+
+const Link = ({ era, gradientStart, gradientEnd }) => {
+  return (
+    <a
+      href={`https://decades.lyricle.app/${era}`}
+      className="flex-grow md:hover:scale-110 md:hover:transition-transform"
+    >
+      <article
+        className={`flex items-center justify-center p-4 bg-gradient-to-r from-[${gradientStart}]/95 to-[${gradientEnd}]/95 aspect-video rounded-md`}
+      >
+        <h1 className="text-indigo-50 text-2xl font-bold">Lyricle {era}</h1>
+      </article>
+    </a>
   )
 }
