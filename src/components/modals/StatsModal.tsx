e5cd1faa9d64a27fb257f093fcc8421dcde238e1
rodas-j <se.rodas.workicho@gmail.com>
2022-08-24 14:54:34 -0400

    Add join our communities to all Decades

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -81,7 +81,7 @@ export const StatsModal = ({
           numberOfGuessesMade={numberOfGuessesMade}
         />
 
-        {decadesConfig.key === '70s' && <JoinOurCommunities />}
+        <JoinOurCommunities />
 
         {(isGameLost || isGameWon) && (
           <>
