1933aa7bff1d7fa55514397e070929c81e55c554
rodas-j <se.rodas.workicho@gmail.com>
2022-09-04 06:29:11 -0400

    Join our community

--- src/components/links/Links.tsx
+++ src/components/links/Links.tsx
@@ -1,13 +1,55 @@
 import { decadesConfig } from '../../lib/config'
+import { ReactComponent as RedditIcon } from '../../assets/reddit.svg'
+import { ReactComponent as DiscordIcon } from '../../assets/discord.svg'
+import { ReactComponent as FacebookIcon } from '../../assets/facebook.svg'
 
 export const Links = () => {
   return (
-    <div className="flex flex-wrap gap-4">
-      <Link era="arianagrande" />
-      <Link era="beatles" />
-      <Link era="edsheeran" />
-      <Link era="prince" />
-      <Link era="taylorswift" />
+    <>
+      <div className="flex flex-wrap gap-4">
+        <Link era="arianagrande" />
+        <Link era="beatles" />
+        <Link era="edsheeran" />
+        <Link era="prince" />
+        <Link era="taylorswift" />
+      </div>
+      {JoinOurCommunities()}
+    </>
+  )
+}
+
+const JoinOurCommunities = () => {
+  return (
+    <div className="flex my-3 items-center bg-indigo-200 outline-dashed outline-2 outline-indigo-900 md:hover:outline-offset-2 dark:bg-indigo-600/20 dark:outline-indigo-400 p-2">
+      <p className="dark:text-white text-sm md:text-base">
+        Join our communities to request for more artists!
+      </p>
+      <div className="flex ml-auto">
+        <a
+          href="https://www.reddit.com/r/lyricle_official/"
+          target="_blank"
+          rel="noreferrer"
+          className="focus-visible:outline-none"
+        >
+          <RedditIcon className="w-6 h-6 mx-1 fill-gray-700 hover:fill-gray-900 dark:fill-gray-100 dark:hover:fill-white" />
+        </a>
+        <a
+          href="https://discord.gg/84WNMqAT"
+          target="_blank"
+          rel="noreferrer"
+          className="focus-visible:outline-none"
+        >
+          <DiscordIcon className="w-6 h-6 mx-1 fill-gray-700 hover:fill-gray-900 dark:fill-gray-100 dark:hover:fill-white" />
+        </a>
+        <a
+          href="https://www.facebook.com/groups/1730611307275095"
+          target="_blank"
+          rel="noreferrer"
+          className="focus-visible:outline-none"
+        >
+          <FacebookIcon className="w-6 h-6 mx-1 fill-gray-700 hover:fill-gray-900 dark:fill-gray-100 dark:hover:fill-white" />
+        </a>
+      </div>
     </div>
   )
 }
@@ -22,13 +64,15 @@ const Link = ({ era }) => {
   }
 
   return (
-    <a
-      href={`https://artists.lyricle.app/${era}`}
-      className={`md:hover:scale-110 md:hover:transition-transform flex-grow flex-shrink-0 flex items-center justify-center w-56 h-40 bg-gradient-to-r rounded-md ${gradientClasses[era]}`}
-    >
-      <h1 className="text-indigo-50 text-2xl font-bold">
-        {decadesConfig.selections[era]}
-      </h1>
-    </a>
+    <>
+      <a
+        href={`https://artists.lyricle.app/${era}`}
+        className={`md:hover:scale-110 md:hover:transition-transform flex-grow flex-shrink-0 flex items-center justify-center w-56 h-40 bg-gradient-to-r rounded-md ${gradientClasses[era]}`}
+      >
+        <h1 className="text-indigo-50 text-2xl font-bold">
+          {decadesConfig.selections[era]}
+        </h1>
+      </a>
+    </>
   )
 }
