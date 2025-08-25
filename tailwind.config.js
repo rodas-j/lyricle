dc71d5aa8793f111745adfe842569573738a35a3
rodas-j <50768698+rodas-j@users.noreply.github.com>
2025-08-25 16:20:10 -0400

    Add custom fonts Poppins and OpenRunde to Tailwind configuration and CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        openrunde: ['OpenRunde', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
